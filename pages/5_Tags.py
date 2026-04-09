b1fe2f4f5a9f842ef03db6d47c3ffa99a2aefe6a
lfs26 <105524961+lfs26@users.noreply.github.com>
2026-04-08 17:33:13 -0700

    Update 5_Tags.py

--- pages/5_Tags.py
+++ pages/5_Tags.py
@@ -11,9 +11,16 @@ tag_description = st.text_area("Description (optional)")
 
 if st.button("Add Tag"):
     execute(
-        "INSERT INTO tags (tag_name, tag_color) VALUES (%s, %s) ON CONFLICT DO NOTHING;",
-        (tag_name, tag_color)
-    )
+    """
+    INSERT INTO tags (tag_name, tag_color, description)
+    VALUES (%s, %s, %s)
+    ON CONFLICT (tag_name) DO UPDATE
+        SET tag_color = EXCLUDED.tag_color,
+            description = EXCLUDED.description;
+    """,
+    (tag_name, tag_color, tag_description)
+)
+
     st.success("Tag added!")
 
 st.markdown("---")
